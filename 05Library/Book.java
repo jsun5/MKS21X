a00a307aa85fdc3732d389a06624869f2bcbe61e
jsun5 <jsun5@stuy.edu>
2017-11-28 14:27:22 -0500

    wrote and added Book.java

---
+++ 05Library/Book.java
@@ -0,0 +1,29 @@
+public class Book{
+    private String author;
+    private String title;
+    private int ISBN;
+
+    public Book() {};
+    
+    public Book(a,t,I){
+	author = a;
+	title = t;
+	ISBN = I;
+    }
+
+    public String getAuthor(){
+	return author;
+    }
+
+    public String getTitle(){
+	return title;
+    }
+
+    public int getISBN(){
+	return ISBN;
+    }
+
+    public String toString(){
+	return title + ", " + author + ", " + ISBN;
+    }
+}
