a4696d246357705d48518350cc440483304105fa
jsun5 <jsun5@stuy.edu>
2017-12-01 22:42:05 -0500

    added main driver, for-each works!

--- 06SuperArrayIterator/SuperArray.java
+++ 06SuperArrayIterator/SuperArray.java
@@ -21,7 +21,7 @@ public class SuperArray implements Iterable<String>{
 	    else {
 		System.exit(0);
 	    }
-	    return data.get(index);
+	    return data.get(index- 1);
 	}
 
 	public boolean hasNext(){
@@ -189,4 +189,26 @@ public class SuperArray implements Iterable<String>{
         }
         return false;
     }
+    
+    ////////////// ITERATOR DRIVER ///////////////
+    public static void main(String[] args) {
+    SuperArray data = new SuperArray();
+    int i = 0;
+    while(i < 26){
+      data.add(""+(char)('A'+i%26));
+      i++;
+    }
+
+    System.out.println(data);
+    System.out.println("Standard loop:");
+
+    for(int n = 0; n < data.size(); n++){
+      System.out.print(data.get(n)+" ");
+    }
+    System.out.println();
+    System.out.println("for-each loop:");
+    for(String s : data){
+      System.out.print(s+" ");
+    }
+  }
 }
