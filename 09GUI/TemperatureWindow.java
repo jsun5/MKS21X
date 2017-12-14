9da9dfc8c0d2dcbc88239b9ac7b9451b340121b8
jsun5 <jsun5@stuy.edu>
2017-12-14 01:10:37 -0500

    working model, 2 buttons and text box

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class TemperatureWindow extends JFrame{
+public class TemperatureWindow extends JFrame implements ActionListener{
     private Container pane;
     private JButton CF;
     private JButton FC;
@@ -9,20 +10,40 @@ public class TemperatureWindow extends JFrame{
 
     public TemperatureWindow(){
 	this.setTitle("Temperature Convertor");
-	this.setSize(600,400);
+	this.setSize(600,200);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
-
 	CF = new JButton("Convert Celsius to Fahrenheit");
 	FC = new JButton("Convert Fahrenheit to Celsius");
 	t = new JTextField(10);
+
+	CF.addActionListener(this);
+	FC.addActionListener(this);
+	t.addActionListener(this);
+	
 	pane.add(t);
 	pane.add(CF);
 	pane.add(FC);
     }
+
+    public void actionPerformed(ActionEvent e){
+	String s = e.getActionCommand();
+	System.out.println(s);
+	if(s.equals("Convert Celsius to Fahrenheit")){
+	    double temp = Double.parseDouble(t.getText());
+	    t.setText(Double.toString(TempConversion.CtoF(temp)));
+	}
+	if(s.equals("Convert Fahrenheit to Celsius")){
+	    double temp = Double.parseDouble(t.getText());
+	    t.setText(Double.toString(TempConversion.FtoC(temp)));
+	}
+    }
+
+
+	    
 
     public static void main(String[]args){
 	TemperatureWindow w = new TemperatureWindow();
